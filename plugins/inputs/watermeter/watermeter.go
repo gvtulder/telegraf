f564cc83f3bc48d6d7cef0d48b34d93db1e993b3
Gijs van Tulder <gijs@vantulder.net>
2024-06-09 14:06:00 +0200

    Retry with go-pigpio.

--- plugins/inputs/watermeter/watermeter.go
+++ plugins/inputs/watermeter/watermeter.go
@@ -9,7 +9,7 @@ import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 
-	"github.com/influxdata/telegraf/plugins/common/pigpiod"
+  "github.com/go-pigpio/pigpio/pigpio"
 )
 
 type Watermeter struct {
